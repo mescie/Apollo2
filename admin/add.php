c0e09e5f8fef113ec1771943445fb3f3ea866d1f
Kevin Savonije <Kevin Savonije@localhost>
2016-03-13 10:39:42 +0100

    Fix add new players

--- admin/add.php
+++ admin/add.php
@@ -7,7 +7,7 @@ $positie        = $_POST['positie'];
 
 if (isset($_POST['save']) && !empty($_POST['naam'])) {
 
-    $sql = "INSERT INTO users (naam, positie)
+    $sql = "INSERT INTO sb_users (naam, positie)
             VALUES ('$naam', '$positie')";
 
     if (!$result = $db->query( $sql )) {
