472e4236d75760286f4ae11ce086c403292a0728
Kevin Savonije <kevin@yoursurprise.com>
2018-10-16 08:22:11 +0000

    db_config_live.php edited online with Bitbucket

--- db_config_live.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db = new mysqli('svapollo69.nl', 'kevin', 'apollo69', 'apollo69');
-
-if($db->connect_errno > 0) {
-    die( 'Unable to connect to database [' . $db->connect_error . ']' );
-}
-
-?>
